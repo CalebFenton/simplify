987d691edd4eb39cb53f427829165a4126a67246
Caleb Fenton <caleb.fenton@lookout.com>
2014-06-17 21:44:48 -0700

    Add move tests

---
+++ resources/test/vm/ops/move_test.smali
@@ -0,0 +1,34 @@
+.class Lmove_test;
+.super Ljava/lang/Object;
+
+.method public static TestMoveRegisterPrimitive()V
+    .locals 2
+
+    move v1, v0
+
+    return-void
+.end method
+
+.method public static TestMoveRegisterObject()V
+    .locals 2
+
+    move v1, v0
+
+    return-void
+.end method
+
+.method public static TestMoveResult()V
+    .locals 1
+
+    move-result v0
+
+    return-void
+.end method
+
+.method public static TestMoveException()V
+    .locals 1
+
+    move-exception v0
+
+    return-void
+.end method
---
+++ test/simplify/vm/ops/TestMoveOp.java
@@ -0,0 +1,71 @@
+package simplify.vm.ops;
+
+import static org.junit.Assert.assertTrue;
+import gnu.trove.list.TIntList;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+import simplify.Main;
+import simplify.vm.ContextGraph;
+import simplify.vm.MethodContext;
+import simplify.vm.VMTester;
+import simplify.vm.types.UnknownValue;
+import util.SparseArray;
+
+public class TestMoveOp {
+
+    private static final Logger log = Logger.getLogger(Main.class.getSimpleName());
+
+    private static final String CLASS_NAME = "Lmove_test;";
+
+    class IdentityTest {
+        public boolean equals(IdentityTest rhs) {
+            System.out.println("ident test " + this + " called " + rhs);
+            return this == rhs;
+        }
+    }
+
+    @Test
+    public void TestMoveRegisterPrimitive() {
+        SparseArray<Object> initial = MethodContext.buildRegisterState(0, 42);
+        SparseArray<Object> expected = MethodContext.buildRegisterState(0, 42, 1, 42);
+
+        VMTester.testState(CLASS_NAME, "TestMoveRegisterPrimitive()V", initial, expected);
+    }
+
+    @Test
+    public void TestMoveRegisterObject() {
+        Thread expect = new Thread();
+        SparseArray<Object> initial = MethodContext.buildRegisterState(0, new Thread());
+
+        // Must invoke VM directly to ensure both registers are identical under "=="
+        ContextGraph graph = VMTester.execute(CLASS_NAME, "TestMoveRegisterObject()V", initial);
+        TIntList addresses = graph.getConnectedTerminatingAddresses();
+        assertTrue("Should terminate when expected: " + addresses + " == {1}",
+                        Arrays.equals(addresses.toArray(), new int[] { 1 }));
+
+        Object register0 = graph.getRegisterConsensus(1, 0);
+        Object register1 = graph.getRegisterConsensus(1, 1);
+
+        assertTrue(register0 == register1);
+        assertTrue(register0 instanceof Thread);
+    }
+
+    @Test
+    public void TestMoveResult() {
+        SparseArray<Object> initial = MethodContext.buildRegisterState(MethodContext.ResultRegister, 42);
+        SparseArray<Object> expected = MethodContext.buildRegisterState(0, 42);
+
+        VMTester.testState(CLASS_NAME, "TestMoveResult()V", initial, expected);
+    }
+
+    @Test
+    public void TestMoveException() {
+        SparseArray<Object> expected = MethodContext.buildRegisterState(0, new UnknownValue("Ljava/lang/Exception;"));
+
+        VMTester.test(CLASS_NAME, "TestMoveException()V", expected);
+    }
+}
