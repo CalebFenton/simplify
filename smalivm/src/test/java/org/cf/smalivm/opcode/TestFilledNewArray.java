3d4b978aab5759c49dd16e558e780dd378943dbf
Caleb Fenton <calebjfenton@gmail.com>
2014-12-16 12:57:09 -0800

    Refactor for style

--- smalivm/src/test/java/org/cf/smalivm/opcode/TestFilledNewArray.java
+++ smalivm/src/test/java/org/cf/smalivm/opcode/TestFilledNewArray.java
@@ -10,24 +10,24 @@ import org.junit.Test;
 public class TestFilledNewArray {
 
     private static final String CLASS_NAME = "Lfilled_new_array_test;";
+    private static final String METHOD_NAME = "TestFilledNewArray()V";
 
     @Test
-    public void testFilledNewArrayOp() {
-        TIntObjectMap<Object> initial = VMTester.buildRegisterState(0, 2, 1, 3, 2, 5);
-	TIntObjectMap<Object> expected = VMTester.buildRegisterState(MethodState.ResultRegister, new int[] { 2, 3, 5 });
+    public void testKnownValuesCreatesArrayWithExpectedContents() {
+        int[] elements = new int[] { 2, 3, 5 };
+        TIntObjectMap<Object> initial = VMTester.buildRegisterState(0, elements[0], 1, elements[1], 2, elements[2]);
+        TIntObjectMap<Object> expected = VMTester.buildRegisterState(MethodState.ResultRegister, elements);
 
-	VMTester.testMethodState(CLASS_NAME, "TestFilledNewArray()V", initial, expected);
+        VMTester.testMethodState(CLASS_NAME, METHOD_NAME, initial, expected);
     }
 
-    /*
-     * Ensure that any UnknownValue in a register causes the return state to be an UnknownValue Integer array
-     */
     @Test
-    public void testFilledNewArrayOpUnknownValue() {
-	TIntObjectMap<Object> initial = VMTester.buildRegisterState(0, 2, 1, 3, 2, new UnknownValue("I"));
+    public void testUnknownElementParameterReturnsUnknownValueOfIntegerArrayType() {
+        TIntObjectMap<Object> initial = VMTester.buildRegisterState(0, 2, 1, 3, 2, new UnknownValue("I"));
         TIntObjectMap<Object> expected = VMTester
-	    .buildRegisterState(MethodState.ResultRegister, new UnknownValue("[I"));
+                        .buildRegisterState(MethodState.ResultRegister, new UnknownValue("[I"));
 
-        VMTester.testMethodState(CLASS_NAME, "TestFilledNewArray()V", initial, expected);
+        VMTester.testMethodState(CLASS_NAME, METHOD_NAME, initial, expected);
     }
+
 }
