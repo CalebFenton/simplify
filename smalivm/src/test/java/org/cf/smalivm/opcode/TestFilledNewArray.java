3a040c73ceccc4f8d3ae44530b48245fac4e9fb1
Tim Strazzere <Tim.Strazzere@mylookout.com>
2014-12-16 12:08:37 -0800

    Prevent UnknownValue class-cast exceptions.
    When encountering an UnknownValue inside a
    filled-new-array, the opcode object should
    return an UnknownValue integer array, otherwise
    dragons can happen.

--- smalivm/src/test/java/org/cf/smalivm/opcode/TestFilledNewArray.java
+++ smalivm/src/test/java/org/cf/smalivm/opcode/TestFilledNewArray.java
@@ -4,6 +4,7 @@ import gnu.trove.map.TIntObjectMap;
 
 import org.cf.smalivm.VMTester;
 import org.cf.smalivm.context.MethodState;
+import org.cf.smalivm.type.UnknownValue;
 import org.junit.Test;
 
 public class TestFilledNewArray {
@@ -13,8 +14,19 @@ public class TestFilledNewArray {
     @Test
     public void testFilledNewArrayOp() {
         TIntObjectMap<Object> initial = VMTester.buildRegisterState(0, 2, 1, 3, 2, 5);
-        TIntObjectMap<Object> expected = VMTester.buildRegisterState(MethodState.ResultRegister,
-                        new int[] { 2, 3, 5 });
+	TIntObjectMap<Object> expected = VMTester.buildRegisterState(MethodState.ResultRegister, new int[] { 2, 3, 5 });
+
+	VMTester.testMethodState(CLASS_NAME, "TestFilledNewArray()V", initial, expected);
+    }
+
+    /*
+     * Ensure that any UnknownValue in a register causes the return state to be an UnknownValue Integer array
+     */
+    @Test
+    public void testFilledNewArrayOpUnknownValue() {
+	TIntObjectMap<Object> initial = VMTester.buildRegisterState(0, 2, 1, 3, 2, new UnknownValue("I"));
+        TIntObjectMap<Object> expected = VMTester
+	    .buildRegisterState(MethodState.ResultRegister, new UnknownValue("[I"));
 
         VMTester.testMethodState(CLASS_NAME, "TestFilledNewArray()V", initial, expected);
     }
