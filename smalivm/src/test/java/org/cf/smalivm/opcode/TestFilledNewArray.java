ea274eb82267e361c804663439c587b1a04d62c8
Caleb Fenton <calebjfenton@gmail.com>
2014-12-21 10:13:05 -0800

    Properly cast integers in even more edge cases

--- smalivm/src/test/java/org/cf/smalivm/opcode/TestFilledNewArray.java
+++ smalivm/src/test/java/org/cf/smalivm/opcode/TestFilledNewArray.java
@@ -13,10 +13,20 @@ public class TestFilledNewArray {
     private static final String METHOD_NAME = "TestFilledNewArray()V";
 
     @Test
-    public void testKnownValuesCreatesArrayWithExpectedContents() {
+    public void testIntegerParametersCreatesArrayWithExpectedContents() {
         int[] elements = new int[] { 2, 3, 5 };
         TIntObjectMap<Object> initial = VMTester.buildRegisterState(0, elements[0], 1, elements[1], 2, elements[2]);
         TIntObjectMap<Object> expected = VMTester.buildRegisterState(MethodState.ResultRegister, elements);
+
+        VMTester.testMethodState(CLASS_NAME, METHOD_NAME, initial, expected);
+    }
+
+    @Test
+    public void testShortParametersCreatesArrayWithExpectedContents() {
+        Short[] elements = new Short[] { 2, 3, 5 };
+        int[] intElements = new int[] { 2, 3, 5 };
+        TIntObjectMap<Object> initial = VMTester.buildRegisterState(0, elements[0], 1, elements[1], 2, elements[2]);
+        TIntObjectMap<Object> expected = VMTester.buildRegisterState(MethodState.ResultRegister, intElements);
 
         VMTester.testMethodState(CLASS_NAME, METHOD_NAME, initial, expected);
     }
