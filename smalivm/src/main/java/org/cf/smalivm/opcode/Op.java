3d583d4078a36ace3942c5e73f3a4fa0b3e9d140
Caleb Fenton <calebjfenton@gmail.com>
2015-03-02 07:34:43 -0800

    Add VirtualExceptions foundation

--- smalivm/src/main/java/org/cf/smalivm/opcode/Op.java
+++ smalivm/src/main/java/org/cf/smalivm/opcode/Op.java
@@ -1,12 +1,14 @@
 package org.cf.smalivm.opcode;
 
 import org.cf.smalivm.SideEffect;
+import org.cf.smalivm.VirtualException;
 
 public abstract class Op {
 
     private int address;
     private int[] childAddresses;
     private final String opName;
+    private VirtualException thrownException;
 
     Op(int address, String opName, int childAddress) {
         this(address, opName, new int[] { childAddress });
@@ -16,6 +18,7 @@ public abstract class Op {
         this.address = address;
         this.opName = opName;
         this.childAddresses = childAddresses;
+        thrownException = null;
     }
 
     public final int getAddress() {
@@ -40,6 +43,14 @@ public abstract class Op {
 
     public void setChildren(int[] childAddresses) {
         this.childAddresses = childAddresses;
+    }
+
+    public VirtualException thrownException() {
+        return thrownException;
+    }
+
+    protected void setException(VirtualException thrownException) {
+        this.thrownException = thrownException;
     }
 
     @Override
