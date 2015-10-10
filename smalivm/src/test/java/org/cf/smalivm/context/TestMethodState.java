f03c8be80ea9a8ad09cce8fc0eeb2e0eb8abdfa4
Caleb Fenton <calebjfenton@gmail.com>
2015-10-10 15:48:55 -0700

    Add MethodState#toString test

--- smalivm/src/test/java/org/cf/smalivm/context/TestMethodState.java
+++ smalivm/src/test/java/org/cf/smalivm/context/TestMethodState.java
@@ -1,5 +1,22 @@
 package org.cf.smalivm.context;
 
+import static org.junit.Assert.assertEquals;
+
+import org.cf.smalivm.VMTester;
+import org.junit.Test;
+
 public class TestMethodState {
 
+    private static final String CLASS_NAME = "Lsimple_loop;";
+
+    @Test
+    public void hasExpectedToString() {
+        ExecutionGraph graph = VMTester.execute(CLASS_NAME, "simpleLoop()V");
+
+        ExecutionNode node = graph.getRoot();
+        MethodState state = node.getContext().getMethodState();
+        String expected = "params: 0, locals: 2\nv0: type=I, value=0";
+
+        assertEquals(expected, state.toString());
+    }
 }
