db252f5765155c1b6c93d5d7029c2161e240b257
Caleb Fenton <calebjfenton@gmail.com>
2015-10-10 15:43:24 -0700

    Bring sanity to MethodState#toString
    Also, move printing graphs to graphviz to printer

---
+++ smalivm/src/test/java/org/cf/smalivm/context/TestExecutionGraphPrinter.java
@@ -0,0 +1,62 @@
+package org.cf.smalivm.context;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class TestExecutionGraphPrinter {
+
+    private static ExecutionNode buildNode(int address, String nodeString, String stateString) {
+        ExecutionNode node = mock(ExecutionNode.class);
+        when(node.getAddress()).thenReturn(address);
+        when(node.toString()).thenReturn(nodeString);
+
+        MethodState state = mock(MethodState.class);
+        when(state.toString()).thenReturn(stateString);
+        ExecutionContext context = mock(ExecutionContext.class);
+        when(context.getMethodState()).thenReturn(state);
+
+        when(node.getContext()).thenReturn(context);
+
+        return node;
+    }
+
+    private static final int CHILD_ADDRESS = 2;
+    private static final String CHILD_NODE_STR = "child node";
+    private static final String CHILD_STATE_STR = "child state";
+
+    private static final int ROOT_ADDRESS = 1;
+    private static final String ROOT_NODE_STR = "root node";
+    private static final String ROOT_STATE_STR = "root state";
+
+    @Test
+    public void testHasExpectedGraph() {
+        ExecutionNode child = buildNode(CHILD_ADDRESS, CHILD_NODE_STR, CHILD_STATE_STR);
+        when(child.getChildren()).thenReturn(new LinkedList<ExecutionNode>());
+
+        ExecutionNode root = buildNode(ROOT_ADDRESS, ROOT_NODE_STR, ROOT_STATE_STR);
+        List<ExecutionNode> children = new LinkedList<ExecutionNode>();
+        children.add(child);
+        when(root.getChildren()).thenReturn(children);
+
+        ExecutionGraph graph = mock(ExecutionGraph.class);
+        when(graph.getRoot()).thenReturn(root);
+
+        String digraph = ExecutionGraphPrinter.print(graph);
+
+        StringBuilder sb = new StringBuilder("digraph {\n");
+        sb.append("\"@").append(ROOT_ADDRESS).append(" - ").append(ROOT_NODE_STR).append('\n');
+        sb.append(ROOT_STATE_STR).append("\" -> ");
+        sb.append("\"@").append(CHILD_ADDRESS).append(" - ").append(CHILD_NODE_STR).append('\n');
+        sb.append(CHILD_STATE_STR).append("\"\n}");
+
+        String expected = sb.toString();
+        assertEquals(expected, digraph);
+    }
+
+}
