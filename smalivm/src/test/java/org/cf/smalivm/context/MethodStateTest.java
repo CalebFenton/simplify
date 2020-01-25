a3949bd42684302062eaefdb2829f3d36d97a28c
Caleb Fenton <calebjfenton@gmail.com>
2020-01-24 18:44:37 -0800

    Add test for method state toString nulls

--- smalivm/src/test/java/org/cf/smalivm/context/MethodStateTest.java
+++ smalivm/src/test/java/org/cf/smalivm/context/MethodStateTest.java
@@ -19,4 +19,16 @@ public class MethodStateTest {
 
         assertEquals(expected, state.toString());
     }
+
+    @Test
+    public void toStringHandlesNullRegisterValuesWhenPeekingAllRegisters() {
+        ExecutionGraph graph = VMTester.execute(CLASS_NAME, "simpleLoop()V");
+
+        ExecutionNode node = graph.getRoot();
+        MethodState state = node.getContext().getMethodState();
+        String expected = "params: 0, locals: 2\nv0: type=I, value=0\nv1: null";
+
+        assertEquals(expected, state.toString(false));
+    }
+
 }
