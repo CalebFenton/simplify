45858cd825b94c5b310e3e8e91fe16ca2b20edfd
Caleb Fenton <calebjfenton@gmail.com>
2020-08-31 15:26:56 -0700

    Update Gradle distribution and switch to AndroidX

--- simplify/ObfuscatedApp/app/src/main/java/org/cf/obfuscated/MainActivity.java
+++ simplify/ObfuscatedApp/app/src/main/java/org/cf/obfuscated/MainActivity.java
@@ -1,7 +1,7 @@
 package org.cf.obfuscated;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
